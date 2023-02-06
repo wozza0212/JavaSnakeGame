e145ad5fdcd9e47b9933766767e37aeae12337ef
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-02-06 19:59:09 +0000

    Finished Main Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello  world!");
+
+        JFrame obj = new JFrame();
+        GamePlay gameplay = new GamePlay();
+
+        obj.setBounds(10, 10, 905, 700); //Game Screen
+        obj.setBackground(Color.DARK_GRAY);
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+
+
     }
 }
