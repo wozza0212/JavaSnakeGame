60d2c9319f6fe4701b044af2bed2cc14b51ec759
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-02-08 20:03:07 +0000

    Working iup to drawing the snake

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -2,6 +2,21 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GamePlay extends JPanel {
+
+    private int [] snakeXlength = new int[750];
+    private int[] snakeYlength = new int[750];
+
+    private boolean left = false;
+    private boolean right = false;
+    private boolean up = false;
+    private boolean down = false;
+
+    private ImageIcon headRight;
+    private ImageIcon headLeft;
+    private ImageIcon headUp;
+    private ImageIcon headDown;
+
+
     private ImageIcon titleImage;
 
     public GamePlay () {
@@ -11,11 +26,11 @@ public class GamePlay extends JPanel {
     public void paint(Graphics g) {
         titleImage = new ImageIcon(".//game_images/SnakeTitle5.png");
         Image image = titleImage.getImage();
-        Image newImage = image.getScaledInstance(851, 100, Image.SCALE_SMOOTH);
+        Image newImage = image.getScaledInstance(851, 120, Image.SCALE_SMOOTH);
         titleImage = new ImageIcon(newImage);
         titleImage.paintIcon(this, g, 25, 5);
 
-        //Dusplay gameplay border
+        //Display gameplay border
         g.setColor(Color.DARK_GRAY);
         g.drawRect(24, 74,  851, 577);
 
