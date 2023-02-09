54ef4a25b623483e33f6f30ad1c3032244897e2f
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2023-02-09 21:52:53 +0000

    swap down picture from right

--- src/GamePlay.java
+++ src/GamePlay.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class GamePlay extends JPanel implements KeyListener, ActionListener {
 
@@ -29,7 +30,19 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
     private int moves = 0;
     private int score = 0;
 
+    private int [] fruitXposition = {25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300,
+            325, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600,
+            625, 650, 675, 700, 725, 750, 775, 800, 825, 850};
 
+    private int [] fruitYposition = {25, 50, 75, 100, 125, 150, 175, 200, 225, 250, 275, 300,
+            325, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 625};
+
+    private ImageIcon fruitImage;
+
+    private Random random = new Random();
+
+    private int xPosition = random.nextInt(34);
+    private int yPosition = random.nextInt(23);
     private ImageIcon titleImage;
 
     public GamePlay () {
